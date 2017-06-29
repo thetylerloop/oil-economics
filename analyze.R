00d9c20733aa27d851718accb43d962d8fb99e98
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-06-29 08:04:38 -0500

    Pull in more data sources.

--- analyze.R
+++ analyze.R
@@ -4,6 +4,16 @@ library(lookupr)
 library(readr)
 library(zoo)
 
+# Sources
+# - Smith County sales tax data: Texas Comptroller (https://mycpa.cpa.state.tx.us/allocation/HistSales)
+# - WTI crude oil prices: Fed FRED series MCOILWTICO
+# - Oil and Gas employment: BLS QCEW series ENU48423105211
+# - Oil and Gas total wages: BLS QCEW series ENU48423305211
+# - Oil and Gas average annual pay: BLS QCEW series ENU48423505211
+# - Smith County employment: BLS QCEW series ENU4842310510
+# - Smith County total wages: BLS QCEW series ENU4842330510
+# - Smith County average annual pay: BLS QCEW series ENU4842350510 
+
 BASE_CPI_YEAR = 2016
 
 us_cpi <- from_lookup("year", "cpi")
@@ -37,7 +47,13 @@ sales_tax <- read_csv("data/sales_tax.csv") %>%
     gross_sales = sum(gross_sales),
     subject_to_tax = sum(subject_to_tax),
     establishments = mean(establishments)
-  )
+  ) %>%
+  left_join(us_cpi, by = c("year" = "year")) %>%
+  mutate(
+    gross_sales = gross_sales * (us_base_cpi / cpi),
+    subject_to_tax = subject_to_tax * (us_base_cpi / cpi)
+  ) %>%
+  select(-cpi)
 
 merged <- sales_tax %>%
   left_join(oil, by = c("year" = "year"))
