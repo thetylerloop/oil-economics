df9abb3c91c8cadaf6a8f502b3d3d98af6b719a1
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-06-30 19:23:13 -0500

    Add inputs to model.

--- analyze.R
+++ analyze.R
@@ -4,103 +4,118 @@ library(lubridate)
 library(lookupr)
 library(readr)
 library(readxl)
+library(stringr)
 library(zoo)
 
 # Sources
+# - CPI, not seasionally adjusted: BLS series CUUR0000SA0
 # - Smith County sales tax data: Texas Comptroller (https://mycpa.cpa.state.tx.us/allocation/HistSales)
 # - WTI crude oil prices: Fed FRED series MCOILWTICO
-# - Oil and Gas employment: BLS QCEW series ENU48423105211
-# - Oil and Gas total wages: BLS QCEW series ENU48423305211
-# - Oil and Gas average annual pay: BLS QCEW series ENU48423505211
-# - Smith County employment: BLS QCEW series ENU4842310510
-# - Smith County total wages: BLS QCEW series ENU4842330510
-# - Smith County average annual pay: BLS QCEW series ENU4842350510
+# - Smith County average weekly wages (private): BLS QCEW series ENU4842340510
 # - Smith County unemployment rate: BLS LAU series LAUCN484230000000003
-# - Smith County employed population: BLS LAU series LAUCN484230000000005
+# - Smith County labor force: BLS LAU series LAUCN484230000000006
 
-BASE_CPI_YEAR = 2016
+# Load CPI
+cpi <- read_excel("data/cpi.xlsx", skip = 11) %>%
+  mutate(
+    month = as.numeric(str_sub(Period, 2, 3)),
+    date = paste(Year, month, 1, sep = "-"),
+    quarter = as.yearqtr(date, format = "%Y-%m-%d")
+  ) %>%
+  # Convert from monthly to quarterly
+  group_by(quarter) %>%
+  summarise(
+    cpi = mean(Value)
+  )
 
-us_cpi <- from_lookup("year", "cpi") %>%
-  mutate(year = as.numeric(year))
-us_base_cpi <- us_cpi[us_cpi$year == BASE_CPI_YEAR, ]$cpi
+base_cpi <- cpi[cpi$quarter == as.yearqtr("2016-01-01", format = "%Y-%m-%d"),]$cpi
 
 # Load oil price data
 oil <- read_csv("data/fred_wti_crude.csv") %>%
   mutate(
-    year = year(as.Date(DATE))
+    quarter = as.yearqtr(as.Date(DATE))
   ) %>%
-  select(
-    year,
-    usd_per_barrel = MCOILWTICO
-  ) %>%
-  # Convert to annual series to remove seasonal effects
-  group_by(year) %>%
+  group_by(quarter) %>%
   summarise(
-    oil_price = mean(usd_per_barrel)
+    oil_price = mean(MCOILWTICO)
   ) %>%
-  left_join(us_cpi, by = c("year" = "year")) %>%
+  left_join(cpi) %>%
   # Adjust to 2016 dollars
   mutate(
-    oil_price = oil_price * (us_base_cpi / cpi)
+    oil_price = oil_price * (base_cpi / cpi)
   ) %>%
   select(-cpi)
   
 # Load sales tax data
 sales_tax <- read_csv("data/sales_tax.csv") %>%
   mutate(
-    year = year(as.Date(date, "%m/%d/%y"))
+    quarter = as.yearqtr(as.Date(date, "%m/%d/%y"))
   ) %>%
-  # Convert to annual series to remove seasonal effects
-  group_by(year) %>%
-  summarise(
-    gross_sales = sum(gross_sales),
-    subject_to_tax = sum(subject_to_tax),
-    establishments = mean(establishments)
-  ) %>%
-  left_join(us_cpi, by = c("year" = "year")) %>%
+  left_join(cpi) %>%
   # Adjust to 2016 dollars
   mutate(
-    gross_sales = gross_sales * (us_base_cpi / cpi),
-    subject_to_tax = subject_to_tax * (us_base_cpi / cpi)
+    gross_sales = gross_sales * (base_cpi / cpi),
+    subject_to_tax = subject_to_tax * (base_cpi / cpi)
+  ) %>%
+  select(quarter, gross_sales, subject_to_tax, establishments)
+
+# Load QCEW data series
+labor_force <- read_excel("data/all_labor_force.xlsx", skip = 11) %>%
+  mutate(
+    month = as.numeric(str_sub(Period, 2, 3)),
+    date = paste(Year, month, 1, sep = "-"),
+    quarter = as.yearqtr(date, format = "%Y-%m-%d")
+  ) %>%
+  # Convert from monthly to quarterly
+  group_by(quarter) %>%
+  summarise(
+    labor_force = mean(Value)
+  )
+
+unemployment_rate <- read_excel("data/all_unemployment_rate.xlsx", skip = 11) %>%
+  mutate(
+    month = as.numeric(str_sub(Period, 2, 3)),
+    date = paste(Year, month, 1, sep = "-"),
+    quarter = as.yearqtr(date, format = "%Y-%m-%d")
+  ) %>%
+  # Convert from monthly to quarterly
+  group_by(quarter) %>%
+  summarise(
+    unemployment_rate = mean(Value)
+  )
+
+weekly_wages <- read_excel("data/all_weekly_wages.xlsx", skip = 11) %>%
+  mutate(
+    qtr = as.numeric(str_sub(Period, 2, 3)),
+    date = paste(Year, qtr, sep = "-"),
+    quarter = as.yearqtr(date, format = "%Y-%q")
+  ) %>%
+  # Convert from monthly to quarterly
+  group_by(quarter) %>%
+  summarise(
+    weekly_wages = mean(Value)
+  ) %>%
+  left_join(cpi) %>%
+  # Adjust to 2016 dollars
+  mutate(
+    weekly_wages = weekly_wages * (base_cpi / cpi)
   ) %>%
   select(-cpi)
 
-# Load QCEW data series
-all_employment <- read_excel("data/all_employment.xlsx", skip = 12) %>%
-  select(year = Year, all_employment = Value)
-
-all_total_wages <- read_excel("data/all_total_wages.xlsx", skip = 12) %>%
-  select(year = Year, all_total_wages = Value)
-
-all_annual_pay <- read_excel("data/all_annual_pay.xlsx", skip = 12) %>%
-  select(year = Year, all_annual_pay = Value)
-
-oil_employment <- read_excel("data/naics_211_employment.xlsx", skip = 12) %>%
-  select(year = Year, oil_employment = Value)
-
-oil_total_wages <- read_excel("data/naics_211_total_wages.xlsx", skip = 12) %>%
-  select(year = Year, oil_total_wages = Value)
-
-oil_annual_pay <- read_excel("data/naics_211_average_annual_pay.xlsx", skip = 12) %>%
-  select(year = Year, oil_annual_pay = Value)
-
-qcew <- all_employment %>%
-  full_join(all_total_wages) %>%
-  full_join(all_annual_pay) %>%
-  full_join(oil_employment) %>%
-  full_join(oil_total_wages) %>%
-  full_join(oil_annual_pay) %>%
+merged <- sales_tax %>%
+  left_join(oil) %>% 
+  left_join(labor_force) %>%
+  left_join(unemployment_rate) %>%
+  left_join(weekly_wages) %>%
+  # Standardize model inputs
   mutate(
-    oil_share_employment = oil_employment / all_employment * 100,
-    oil_share_total_wages = oil_total_wages / all_total_wages * 100
+    gross_sales = gross_sales / 1000000,
+    subject_to_tax = subject_to_tax / 1000000,
+    labor_force = labor_force / 1000
   )
 
-merged <- sales_tax %>%
-  left_join(oil, by = "year") %>%
-  left_join(qcew, by = "year")
+model <- lm(subject_to_tax ~ labor_force + unemployment_rate + oil_price + weekly_wages, merged)
+summary(model)  
 
-model <- lm(subject_to_tax ~ all_total_wages, data = merged)
-
-ggplot(merged, aes(x = subject_to_tax, y = oil_price, label = year)) +
-  geom_point() +
-  geom_text(nudge_y = 3)
+ggplot(merged, aes(x = subject_to_tax, y = unemployment_rate)) +
+  geom_point()
